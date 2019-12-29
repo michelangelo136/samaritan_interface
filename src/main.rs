4bf2054888111c454298eb14d1282cab93df93bd
Michael Angel Papamihalis <mixailaggelos136@gmail.com>
2019-12-29 23:31:44 +0000

    added documentation + some minor bug & warnings fixes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 extern crate term_size;
 //use ansi_term::Style;
-use std::io::{stdout, Write};
+//use std::io::{stdout, Write};
 //use std::time;
 use std::thread;
 use termion::{color, cursor, clear, style};
@@ -24,16 +24,19 @@ macro_rules! debug_println {
 
 
 fn main() {
-for x in 0..10 {
-    if let Some((w, h)) = term_size::dimensions() {
+   println!("{}",
+   clear = clear::All); // Clears screen
+
+for _t in 0..10 { // Loops for 10 times
+    if let Some((w, h)) = term_size::dimensions() { // Gets terminal dimensions
         
-    let mut hight = h / 2;
-    let mut weight = w / 2;
+    let hight = h / 2; // Sets hight/weight and sends it to feedback
+    let weight = w / 2;
     feedback(weight, hight);
     }
 
 
-fn feedback(x: usize, y: usize) {
+fn feedback(x: usize, y: usize) { // Gets cordinents (x, y) and sets the censor
    x as i32;
    y as i32;
    let  x1: u16 = x as u16;
@@ -44,7 +47,7 @@ fn feedback(x: usize, y: usize) {
    underline = style::Underline,
    input_x = x,
    input_y = y);
-   println!("{} {}", x, y);
+   debug_println!("{} {}", x, y);
 
 }
    
