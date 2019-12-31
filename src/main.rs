bfedd8f88e813b4fd70dfdcdbeb26c495e6c6ce2
Michael Angel Papamihalis <mixailaggelos136@gmail.com>
2019-12-31 02:34:35 +0000

    minor bug fixes, i thing, added more comments, added stuff to do after file instalation 'unfinished'. Last commit of 2019, stand by for 2020, see you there.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::thread;
 use termion::{color, cursor, clear, style};
 use std::io::prelude::*;
 use std::fs::File;
-use std::mem;
+use std::io::prelude::*;
 
 
 
@@ -27,6 +27,7 @@ macro_rules! debug_println {
 fn main() {
    println!("{}",
    clear = clear::All);                                    // Clears screen
+   
 
  for _t in 0..10 {                                       // Loops for 10 times
      if let Some((w, h)) = term_size::dimensions() {    // Gets terminal dimensions
@@ -35,12 +36,14 @@ fn main() {
      let weight = w / 2;                             // set weght and sends it to debug_size
      //debug_size(weight, hight);                   // Prints dimensions
      //stand_by(weight, hight);                    // Sends parameters to stand_by
-     
-     let mut file = File::open("input/input_file").expect("Unable to open th file"); // Gets input from input_file and converts it to string
+     //awaiting(weight, hight);
+
+     let mut file = File::open("input/input.txt").expect("Unable to open th file"); // Gets input from input_file and converts it to string
      let mut contents = String::new();
      file.read_to_string(&mut contents).expect("Unable to read the file");
      
-     if (contents == " ") {
+     if contents == " " {                     // Checks if the file has commands in it, then start a task
+       awaiting(weight, hight);
      } else {
        input(weight, hight, &contents);
      }
@@ -71,9 +74,9 @@ fn debug_size(x: usize, y: usize) { // Gets cordinents (x, y) and sets the censo
   /* println!("{goto}{red}^",
   red = color::Fg(color::Red),
   goto = cursor::Goto(x1, y1 - 1)); */
-  thread::sleep_ms(2000);
-  println!("{clear}",
-  clear = clear::All);
+  std::thread::sleep_ms(2000);
+  //println!("{clear}",
+  //clear = clear::All);
   
  }
 
@@ -99,20 +102,49 @@ fn input(x: usize, y: usize, contents: &str) { // Prints the input
    let x1: u16 = x as u16;
    let y1: u16 = y as u16;
    //let input1 = "test";
-   let actuall_lenght = contents.len();
+   for splited_input in contents.split_whitespace() {
+   thread::sleep_ms(1250);
+   println!("{clear}",
+   clear = clear::All);
+   
+   //let actuall_lenght = contents.len();
+   //let half_lenght = (actuall_lenght - 1) / 2;
+   //let half_lenght_u16: u16 = half_lenght as u16;
+   
+   let actuall_lenght = splited_input.len();
    let half_lenght = (actuall_lenght - 1) / 2;
-   let half_lenght_u16: u16 = half_lenght as u16;  
+   let half_lenght_u16: u16 = half_lenght as u16;
+   
 
-   println!("{goto_1}{underline} {input_0}{reset_style}{input_1}{goto_2}{red}^{reset_color}",
+   println!("{goto_1}{underline}{input_0}{reset_style}{goto_2}{red}^{reset_color}",
    goto_1 = cursor::Goto(x1 - half_lenght_u16, y1),
    underline = style::Underline,
    reset_style = style::Reset,
    reset_color = color::Fg(color::Reset),
    red = color::Fg(color::Red),
    goto_2 = cursor::Goto(x1, y1 + 1),
-   input_0 = contents,
-   input_1 = actuall_lenght -1 );
+   input_0 = splited_input/*.to_owned() + " "*/);
+   //input_1 = actuall_lenght -1 );
+   debug_println!("**Debug: Actuall lenght {}",
+   actuall_lenght - 1);
+   
+  }
+file_input.write_all(" "); 
+}
 
+fn awaiting (x: usize, y: usize) { // Set's stand_by ststus
+   //x as i32;
+   //y as i32;
+   let x1: u16 = x as u16;
+   let y1: u16 = y as u16;
+
+   println!("{goto_1}AWAITIN{underline}G INSTR{reset_style}UCTIONS{goto_2}{red}^{reset_color}",
+   goto_1 = cursor::Goto(x1 - 10, y1),
+   underline = style::Underline,
+   reset_style = style::Reset,
+   reset_color = color::Fg(color::Reset),
+   red = color::Fg(color::Red),
+   goto_2 = cursor::Goto(x1, y1 + 1));
 
 
 }
